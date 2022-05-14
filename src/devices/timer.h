3fef8e97bcc0e7501fcdc3221b79d4a6b629c276
Zyad Samy <zyadsamysobky@gmail.com>
2022-05-14 19:01:02 +0200

    intial commit

---
+++ src/devices/timer.h
@@ -0,0 +1,40 @@
+#ifndef DEVICES_TIMER_H
+#define DEVICES_TIMER_H
+
+#include <round.h>
+#include <stdint.h>
+#include "lib/kernel/list.h"
+
+
+/* A struct to keep track of sleeping thread with its wake time. */
+struct sleeping_thread
+   {
+      struct list_elem sleeping_elem;
+      struct thread* thread_p;
+      struct semaphore* sema;
+      int64_t wake_time;  
+   };
+
+/* Number of timer interrupts per second. */
+#define TIMER_FREQ 100
+
+void timer_init (void);
+void timer_calibrate (void);
+
+int64_t timer_ticks (void);
+int64_t timer_elapsed (int64_t);
+
+/* Sleep and yield the CPU to other threads. */
+void timer_sleep (int64_t ticks);
+void timer_msleep (int64_t milliseconds);
+void timer_usleep (int64_t microseconds);
+void timer_nsleep (int64_t nanoseconds);
+
+/* Busy waits. */
+void timer_mdelay (int64_t milliseconds);
+void timer_udelay (int64_t microseconds);
+void timer_ndelay (int64_t nanoseconds);
+
+void timer_print_stats (void);
+
+#endif /* devices/timer.h */
