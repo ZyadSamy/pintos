93a53d1c13bbd1b044a07d390d72f6ccb4a73984
Zyad Samy <zyadsamysobky@gmail.com>
2022-05-19 14:46:09 +0200

    .

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -230,7 +230,7 @@ lock_acquire (struct lock *lock)
   ASSERT (!intr_context ());
   ASSERT (!lock_held_by_current_thread (lock));
 
-
+  // printf("hehahah\n");
   bool cannot_acquire = lock -> holder == NULL ? false: true;
 
   if (!thread_mlfqs && cannot_acquire) {
